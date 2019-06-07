a86330fda3f62bb35b95fc93994ff41178873abc
Athoug Alsoughayer <athoug2.s@gmail.com>
2019-06-07 12:40:01 +0300

    Add a ServiceWorker script

---
+++ sw.js
@@ -0,0 +1,59 @@
+// 1. register a service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/sw.js').then(function (registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function (err) {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+// 2. Install the service worker
+// i. Open a cache
+const CACHE_NAME = 'restaurant-review-cache-v1';
+// ii. Cache our files
+const urlsToCache = [
+  '/',
+  '/css/styles.css',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg',
+];
+
+self.addEventListener('install', function (event) {
+  // iii. Confirm whether all the required assets are cached or not
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 3. cache responses
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+      )
+  );
+});
